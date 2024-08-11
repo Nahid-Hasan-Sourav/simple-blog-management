4d64c3bb234900efd5774a9f55eb0649884c13f9
Nahid Hasan Sourav <nahidhasansourav@gmail.com>
2024-08-12 01:13:42 +0600

    feature/dashboard-template: complete dashboard mastering

---
+++ resources/views/admin/layouts/includes/leftsidebar.blade.php
@@ -0,0 +1,31 @@
+<div class="sl-logo"><a href="">DASHBOARD</a></div>
+<div class="sl-sideleft">
+  <div class="card">
+    <div class="">
+        <div class="">
+            <div class="text-center">
+                <img src="{{asset('/')}}admin/assets/img/img9.jpg" class="" alt="..." style="width:100px;border-radius:50%;height:100px;">
+              </div>
+            <h5>Name : {{ Auth::user()->name }}</h5>
+        </div>
+    </div>
+  </div>
+
+{{--  <div class="sl-sideleft-menu">--}}
+{{--    <a href="{{ route('blog.index') }}" class="sl-menu-link {{ request()->is('blog*') ? 'active' : '' }}">--}}
+{{--      <div class="sl-menu-item">--}}
+{{--        <span class="menu-item-label">Blog</span>--}}
+{{--      </div>--}}
+{{--    </a>--}}
+{{--    --}}
+{{--    <a href="{{ route('home') }}" class="sl-menu-link ">--}}
+{{--      <div class="sl-menu-item">--}}
+{{--        <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>--}}
+{{--        <span class="menu-item-label">Home</span>--}}
+{{--      </div>--}}
+{{--    </a>--}}
+
+{{--  </div>--}}
+
+  <br>
+</div>
