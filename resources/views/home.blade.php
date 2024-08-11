35597a1bfae2c4c16c6ad02ef2da7333d8f4de82
Nahid Hasan Sourav <nahidhasansourav@gmail.com>
2024-08-12 00:52:22 +0600

    feature/frontend-template: complete blade mastering for frontend and changes on login,registration blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.app')
+
+@section('body')
+    <style>
+
+        .carousel-caption {
+            top: 50% !important;
+            left: 50% !important;
+            transform: translate(-50%, -50%) !important;
+        }
+        .carousel-inner{
+            height: 500px !important;
+        }
+    </style>
+
+    <div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel" data-mdb-carousel-init>
+        <!-- Indicators -->
+        <div class="carousel-indicators">
+            <button
+                type="button"
+                data-mdb-target="#carouselBasicExample"
+                data-mdb-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+            ></button>
+            <button
+                type="button"
+                data-mdb-target="#carouselBasicExample"
+                data-mdb-slide-to="1"
+                aria-label="Slide 2"
+            ></button>
+            <button
+                type="button"
+                data-mdb-target="#carouselBasicExample"
+                data-mdb-slide-to="2"
+                aria-label="Slide 3"
+            ></button>
+        </div>
+
+        <!-- Inner -->
+        <div class="carousel-inner">
+            <!-- Single item -->
+            <div class="carousel-item active">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="Sunset Over the City"/>
+                <div class="carousel-caption   d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+
+            <!-- Single item -->
+            <div class="carousel-item">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100" alt="Canyon at Nigh"/>
+                <div class="carousel-caption   d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+
+            <!-- Single item -->
+            <div class="carousel-item">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100" alt="Cliff Above a Stormy Sea"/>
+                <div class="carousel-caption   d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Inner -->
+
+        <!-- Controls -->
+        <button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        <!-- Carousel wrapper -->
+    </div>
+    <div class="text-center p-4 text-decoration-underline">
+        <h3>ALL BLOGS</h3>
+    </div>
+    <div class="container-fluid px-0 mt-5" >
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+
+{{--            @forelse($blogs as $blog)--}}
+{{--                <div class="col">--}}
+{{--                    <div class="card">--}}
+{{--                        <img src="{{ $blog->featured_image }}" class="card-img-top" style="height:250px" alt="Fissure in Sandstone">--}}
+{{--                        <div class="card-body">--}}
+{{--                            <h5 class="card-title">{{ $blog->title }}</h5>--}}
+
+{{--                            <p>{{ \Illuminate\Support\Str::limit(strip_tags($blog->content), 100) }}</p>--}}
+
+{{--                            <a href="{{ route('blog.details',['id'=>$blog->id]) }}" class="btn btn-primary" data-mdb-ripple-init>Details</a>--}}
+{{--                            --}}{{-- <p>{!! $blog->content !!}</p>       --}}
+
+
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                @empty--}}
+{{--                <p>No Blogs Found</p>--}}
+
+{{--            @endforelse--}}
+
+            <!-- Card 1 -->
+
+
+
+        </div>
+    </div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,61 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog Website</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- MDB JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.2.0/mdb.min.js"></script>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+</head>
+<body>
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+{{--            <a class="navbar-brand" href="{{ route('home') }}">BlogSite</a>--}}
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="mb-2 navbar-nav me-auto mb-lg-0">
+                    <li class="nav-item">
+                        {{-- <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a> --}}
+                    </li>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+
+                </ul>
+                <div class="d-flex" role="search">
+                    <p class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('dashboard') }}">Dashboard</a>
+                    </p>
+                     <p class="mx-4 nav-item">
+                        @if(auth()->check())
+                            <a id="logoutButton" a class="nav-link active" href="" onclick="event.preventDefault(); document.getElementById('logoutForms').submit()">
+                                Logout
+                            </a>
+
+                    <form id="logoutForms" method="post" action="{{ route('logout') }}">
+                        @csrf
+                    </form>
+                    @else
+                        <a class="nav-link active" aria-current="page" href="{{ route('login') }}">Login</a>
+                        @endif
+                        </p>
+
+                </div>
+            </div>
         </div>
-    </body>
+    </nav>
+    <div >
+        @yield('body')
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
 </html>
